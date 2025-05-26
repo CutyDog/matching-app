d763fa69c9d1cecf8f7258f0ac02435243ae7c4b
CutyDog <silver.soul.818@gmail.com>
2025-05-26 17:54:43 +0900

    add jwt methods, add user_spec

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -21,4 +21,24 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
+
+  class UnauthorizedError < StandardError; end
+
+  def self.find_by_jwt!(jwt_token)
+    secret_key = Rails.application.credentials.secret_key_base
+    payload = JWT.decode(jwt_token, secret_key)[0]
+    User.find(payload['user_id'])
+  rescue JWT::ExpiredSignature
+    raise UnauthorizedError, 'JWT token expired'
+  rescue JWT::DecodeError
+    raise UnauthorizedError, 'Invalid JWT token'
+  rescue ActiveRecord::RecordNotFound
+    raise UnauthorizedError, 'User not found'
+  end
+
+  def issue_jwt_token
+    payload = { user_id: id, exp: 6.hours.from_now.to_i }
+    secret_key = Rails.application.credentials.secret_key_base
+    JWT.encode(payload, secret_key)
+  end
 end
--- server/spec/models/user_spec.rb
+++ server/spec/models/user_spec.rb
@@ -18,5 +18,71 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email) }
+    it { is_expected.to validate_presence_of(:password) }
+  end
+
+  describe '.authentication' do
+    let(:user) { create(:user, email: 'test@example.com', password: 'password') }
+    subject { user.authenticate(password) }
+
+    context 'when password is correct' do
+      let(:password) { 'password' }
+      it do
+        expect(subject).to be_truthy
+      end
+    end
+
+    context 'when password is incorrect' do
+      let(:password) { 'wrong_password' }
+      it do
+        expect(subject).to be_falsey
+      end
+    end
+  end
+
+  describe '.find_by_jwt!' do
+    let(:user) { create(:user, email: 'test@example.com', password: 'password') }
+    subject { User.find_by_jwt!(jwt_token) }
+
+    context 'when the JWT token is valid' do
+      let(:jwt_token) { user.issue_jwt_token }
+      it do
+        expect(subject).to eq(user)
+      end
+    end
+
+    context 'when the JWT token cannot be decoded' do
+      let(:jwt_token) { 'invalid_token' }
+      it do
+        expect { subject }.to raise_error(User::UnauthorizedError, 'Invalid JWT token')
+      end
+    end
+
+    context 'when the JWT token is expired' do
+      let(:jwt_token) do
+        JWT.encode(
+          { user_id: user.id, exp: 1.hour.ago.to_i },
+          Rails.application.credentials.secret_key_base
+        )
+      end
+      it do
+        expect { subject }.to raise_error(User::UnauthorizedError, 'JWT token expired')
+      end
+    end
+
+    context 'when user is not found' do
+      before do
+        allow(User).to receive(:find).and_raise(ActiveRecord::RecordNotFound)
+      end
+      let(:jwt_token) { user.issue_jwt_token }
+      it do
+        expect { subject }.to raise_error(User::UnauthorizedError, 'User not found')
+      end
+    end
+  end
 end
