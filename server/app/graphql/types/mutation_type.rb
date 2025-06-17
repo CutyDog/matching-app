d20fe46a9d367deb75ccf5a03e461f6fcf51da52
CutyDog <silver.soul.818@gmail.com>
2025-06-17 19:34:53 +0900

    add graphiql-rails gem

--- server/app/graphql/types/mutation_type.rb
+++ server/app/graphql/types/mutation_type.rb
@@ -2,5 +2,12 @@
 
 module Types
   class MutationType < Types::BaseObject
+    field :sample, String, null: false do
+      argument :name, String, required: true
+    end
+
+    def sample(name:)
+      "Hello, #{name}!"
+    end
   end
 end
--- server/app/graphql/types/query_type.rb
+++ server/app/graphql/types/query_type.rb
@@ -2,6 +2,14 @@
 
 module Types
   class QueryType < Types::BaseObject
+    field :sample, String, null: false do
+      argument :name, String, required: true
+    end
+
+    def sample(name:)
+      "Hello, #{name}!"
+    end
+
     field :node, Types::NodeType, null: true, description: 'Fetches an object given its ID.' do
       argument :id, ID, required: true, description: 'ID of the object.'
     end
