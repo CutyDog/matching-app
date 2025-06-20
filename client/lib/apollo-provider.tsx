7a02a97091d79d4a1223c12a7ecb4b74e4c987b9
CutyDog <silver.soul.818@gmail.com>
2025-06-20 16:53:38 +0900

    modify apollo-provider

--- client/lib/apollo-provider.tsx
+++ client/lib/apollo-provider.tsx
@@ -1,9 +1,26 @@
 'use client';
 
-import { ApolloProvider } from '@apollo/client';
-import { client } from '../graphql/client';
+import { ApolloClient, ApolloLink, InMemoryCache, HttpLink, ApolloProvider } from '@apollo/client';
 
-export function ApolloWrapper({ children }: { children: React.ReactNode }) {
+const authLink = new ApolloLink((operation, forward) => {
+  operation.setContext(({ headers }: { headers: Record<string, string> }) => ({
+    headers: {
+      Token: typeof window !== 'undefined' ? localStorage.getItem('Token') : null,
+      ...headers,
+    },
+  }));
+
+  return forward(operation);
+});
+const httpLink = new HttpLink({
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT || 'http://localhost:3010/graphql',
+});
+const client = new ApolloClient({
+  link: ApolloLink.from([authLink, httpLink]),
+  cache: new InMemoryCache(),
+});
+
+export const ApolloWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <ApolloProvider client={client}>
       {children}
