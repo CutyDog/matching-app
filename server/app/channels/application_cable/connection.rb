833a9de3841f9668462033b01c4b261903d6f663
CutyDog <silver.soul.818@gmail.com>
2025-06-22 14:03:16 +0900

    add graphql_channel

--- server/app/channels/application_cable/connection.rb
+++ server/app/channels/application_cable/connection.rb
@@ -1,4 +1,21 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+
+    def current_user
+      @current_user ||= User.find_by_jwt!(jwt_token) # rubocop:disable Rails/DynamicFindBy
+    rescue User::UnauthorizedError
+      nil
+    end
+
+    def jwt_token
+      request.headers['Token']&.split(' ')&.last
+    end
   end
 end
